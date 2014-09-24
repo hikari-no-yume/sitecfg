c5b8027932ea6bdec1c4e0bea00c0e84bbcbcf48
Andrea Faulds <ajf@ajf.me>
2014-09-24 18:02:49 +0100

    Initial commit

---
+++ sitecfg.php
@@ -0,0 +1,70 @@
+#!/usr/bin/env php
+<?php
+
+function run($command) {
+    $output = [];
+    $exitstatus = NULL;
+    exec($command, $output, $exitstatus);
+    echo implode("\n", $output), "\n";
+    return !$exitstatus;
+}
+
+$config = file_get_contents("config.json") or die("failed to load config.json\n");
+$config = json_decode($config, true) or die("failed to decode json\n");
+
+function setupSite($site, $siteName) {
+    global $config;
+
+    $siteDir = "$config[sitesDir]/$siteName";
+    run("git clone $site[gitRepo] '$siteDir'") or die("failed to clone git repo\n");
+    run("chown -R $config[owner]:$config[group] '$siteDir'") or die("failed to change owner and group of directory\n");
+    run("ln -s '$siteDir/$site[webRoot]' '/var/www/$siteName'") or die ("failed to symlink webroot into /var/www");
+    run("ln -s '$siteDir/$site[nginxConfigFile]' '/etc/nginx/sites-available/$siteName'") or die("failed to simlink site config file into nginx sites-available\n");
+    run("ln -s '/etc/nginx/sites-available/$siteName' '/etc/nginx/sites-enabled/$siteName'") or die("failed to simlink nginx sites-available to nginx sites-enabled\n");
+    run("service nginx reload") or die("failed to reload nginx\n");
+}
+
+function printUsage() {
+    echo "php sitecfg.php <command> <arguments>\n";
+    echo "Commands:\n";
+    echo "    setupall - Sets up all sites\n";
+    echo "    setup <sitename> - Sets up the named site\n";
+    echo "    teardown <sitename> - Tears down the named site\n";
+}
+
+if ($argc < 2) {
+    printUsage();
+    die();
+}
+
+$sites = file_get_contents("sites.json") or die("couldn't load sites.json\n");
+$sites = json_decode($sites, true) or die("couldn't decode json\n");
+
+switch ($command = $argv[1]) {
+    case 'setupall':
+        foreach ($sites as $siteName => $site) {
+            setupSite($site, $siteName);
+        }
+        break;
+    case 'setup':
+    case 'teardown':
+        if ($argc < 3) {
+            printUsage();
+            die();
+        }
+        $siteName = $argc[2];
+        if (!isset($sites[$siteName])) {
+            die("No such site \"$sitename\"\n");
+        }
+        $site = $sites[$siteName];
+        if ($command === 'setup') {
+            setupSite($site, $siteName);
+            break;
+        } else if ($command === 'teardown') {
+            teardownSite($site);
+            break;
+        }
+    default:
+        printUsage();
+        die();
+}
