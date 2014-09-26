1d9b8448121caf5daee16b84115ba49e93f334a3
Andrea Faulds <ajf@ajf.me>
2014-09-26 14:45:16 +0100

    User sites

--- README.md
+++ README.md
@@ -10,13 +10,15 @@ Configuration
 
 There are two configuration files. The first is `config.json` (copy `config.example.json` to get started), which contains these keys:
 
-* `"owner"` and `"group"` - The username (probably your username) and group name (most likely `www-data` or `nginx`) that the git repository will be `chmod`ed to be owned by
+* `"owner"` and `"group"` - The username (probably your username) and group name (most likely `www-data` or `nginx`) that the git repository will be `chmod`ed to be owned by (`"owner"` isused only for `"git"` sites, `"user"` sites are chowned to their user)
 * `"sitesDir"` - The directory that the git repositories will be put in, e.g. `/home/your_username/sites`
 
 The second is `sites.json` (copy `sites.example.json` to get started), which contains a key for each site. Each site's object contains these keys:
 
-* `"gitRepo"` - The URL of a git repository which will be cloned (from GitHub, for example)
-* `"nginxConfigFile"` - The path within that git repository of the nginx configuration file (which should use `/var/www/site_name` for its webroot), e.g. `example.com.cfg`
-* `"webroot"` - The path within that git repository of the webroot, e.g. `htdocs` (`/var/www/site_name` will be symlinked to point to this)
+* `"type"` - Either `"git"` if we're using a git repo for our site, or `"user"` if we want to create a user account and an empty htdocs directory in its home directory (so someone can SSH in and upload site content)
+* `"gitRepo"` - (`"git"` sites only) The URL of a git repository which will be cloned (from GitHub, for example)
+* `"user"` - (`"user"` sites only) The username to give the user account
+* `"nginxConfigFile"` - The path of the nginx configuration file (which should use `/var/www/site_name` for its webroot) - for `"git"` sites this is relative to the repository e.g. `example.com.cfg`, and for `"user"` sites this is absolute, e.g. `/home/john_appleseed/sites/example.com.cfg`
+* `"webroot"` - (`"git"` sites only - `"user"` sites always create an htdocs directory) The path within that git repository of the webroot, e.g. `htdocs` (`/var/www/site_name` will be symlinked to point to this)
 
 That's it! 
--- sitecfg.php
+++ sitecfg.php
@@ -15,19 +15,33 @@ $config = json_decode($config, true) or die("failed to decode json\n");
 function setupSite($site, $siteName) {
     global $config;
 
-    $siteDir = "$config[sitesDir]/$siteName";
-    run("git clone $site[gitRepo] '$siteDir'") or die("failed to clone git repo\n");
-    run("chown -R $config[owner]:$config[group] '$siteDir'") or die("failed to change owner and group of directory\n");
-    
+    if ($site['type'] === 'git') {
+        $siteDir = "$config[sitesDir]/$siteName";
+        run("git clone $site[gitRepo] '$siteDir'") or die("failed to clone git repo\n");
+    } else if ($site['type'] === 'user') {
+        $siteDir = "/home/$site[user]/htdocs";
+        run("useradd $site[user] --create-home") or die("failed to create user account \"$site[user]\"\n");
+        mkdir($siteDir) or die("failed to create htdocs directory\n");
+    } else {
+        die("unknown site type $site[type]\n");
+    }
+
+    $owner = ($site['type'] === 'git') ? $config['owner'] : $site['user'];
+    run("chown -R $owner:$config[group] '$siteDir'") or die("failed to change owner and group of directory\n");
+        
     if (!file_exists("/var/www")) {
         mkdir("/var/www") or die("couldn't create /var/www\n");
         chgrp("/var/www", $config['group']) or die("failed to chown /var/www");
     }
     
     $wwwDir = "/var/www/$siteName";
-run("ln -s '$siteDir/$site[webRoot]' '$wwwDir'") or die ("failed to symlink webroot into /var/www");
+    $webRoot = ($site['type'] === 'git') ? "$siteDir/$site[webRoot]" : $siteDir;
+
+    run("ln -s '$webRoot' '$wwwDir'") or die ("failed to symlink webroot into /var/www");
     
-    run("ln -s '$siteDir/$site[nginxConfigFile]' '/etc/nginx/sites-available/$siteName'") or die("failed to simlink site config file into nginx sites-available\n");
+    $configFile = ($site['type'] === 'git') ? "$siteDir/$site[nginxConfigFile]" : $site['nginxConfigFile'];
+    
+    run("ln -s '$configFile]' '/etc/nginx/sites-available/$siteName'") or die("failed to simlink site config file into nginx sites-available\n");
     run("ln -s '/etc/nginx/sites-available/$siteName' '/etc/nginx/sites-enabled/$siteName'") or die("failed to simlink nginx sites-available to nginx sites-enabled\n");
     run("service nginx reload") or die("failed to reload nginx\n");
     
@@ -66,9 +80,9 @@ switch ($command = $argv[1]) {
             printUsage();
             die();
         }
-        $siteName = $argc[2];
+        $siteName = $argv[2];
         if (!isset($sites[$siteName])) {
-            die("No such site \"$sitename\"\n");
+            die("No such site \"$siteName\"\n");
         }
         $site = $sites[$siteName];
         if ($command === 'setup') {
